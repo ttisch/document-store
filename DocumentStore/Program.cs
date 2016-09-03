8ff73d7bf6da89adcf2ebfd4534f7212bf7f7a07
beemwe <t.tisch@live.com>
2016-09-03 21:36:00 +0200

    initially added web files

---
+++ DocumentStore/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Add reference to:
+using Microsoft.Owin.Hosting;
+using System.Data.Entity;
+using MinimalOwinWebApiSelfHost.Models;
+
+namespace MinimalOwinWebApiSelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set up and seed the database:
+            Console.WriteLine("Initializing and seeding database...");
+            Database.SetInitializer(new ApplicationDbInitializer());
+            var db = new ApplicationDbContext();
+            int count = db.Companies.Count();
+            Console.WriteLine("Database created and seeded with {0} company records...", count);
+
+
+            // Specify the URI to use for the local host:
+            string baseUri = "http://localhost:12345";
+
+            Console.WriteLine("Starting web Server...");
+            WebApp.Start<Startup>(baseUri);
+            Console.WriteLine("Server running at {0} - press Enter to quit. ", baseUri);
+            Console.ReadLine();
+        }
+    }
+}
