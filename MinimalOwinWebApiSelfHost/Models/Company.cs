da453afe21550b2fc2969bb17590ff7b8facb334
Thomas Tisch <t.tisch@live.com>
2016-09-03 20:03:34 +0200

    added further initial files

---
+++ MinimalOwinWebApiSelfHost/Models/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Add using:
+using System.ComponentModel.DataAnnotations;
+
+namespace MinimalOwinWebApiSelfHost.Models
+{
+    public class Company
+    {
+        // Add Key Attribute:
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ MinimalOwinWebApiSelfHost/Startup.cs
@@ -0,0 +1,45 @@
+﻿
+// Add the following usings:
+using Microsoft.Owin;
+using Microsoft.Owin.Extensions;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+using System.Web.Http;
+
+namespace MinimalOwinWebApiSelfHost
+{
+    public class Startup
+    {
+        // This method is required by Katana:
+        public void Configuration(IAppBuilder app)
+        {
+            var webApiConfiguration = ConfigureWebApi();
+
+            // Use the extension method provided by the WebApi.Owin library:
+            app.UseWebApi(webApiConfiguration);
+
+            app.UseFileServer(new FileServerOptions
+            {
+                RequestPath = new PathString(string.Empty),
+                FileSystem = new PhysicalFileSystem("./public"),
+                EnableDirectoryBrowsing = true
+            });
+
+            app.UseStageMarker(PipelineStage.MapHandler);
+        }
+
+
+        private HttpConfiguration ConfigureWebApi()
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                "DefaultApi",
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+
+            return config;
+        }
+    }
+}
